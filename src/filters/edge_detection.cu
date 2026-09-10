f39d0ac6b7190fa6950096cd1fdaaa481a414307
Samantha Montgomery <32003691+montgomerysamantha@users.noreply.github.com>
2026-09-09 19:07:27 -0700

    Start sobel kernel

--- src/filters/edge_detection.cu
+++ src/filters/edge_detection.cu
@@ -1,7 +1,52 @@
 #include "filters/edge_detection.hpp"
 
+#include "core/cuda_check.hpp"
+
 #include <stdexcept>
 
+namespace
+{
+
+__constant__ int sobelXDevice[3][3] =
+{
+    {-1, 0, 1},
+    {-2, 0, 2},
+    {-1, 0, 1}
+};
+
+__constant__ int sobelYDevice[3][3] =
+{
+    {-1, -2, -1},
+    { 0,  0,  0},
+    { 1,  2,  1}
+};
+
+__device__ void writeRgbDevice(
+    unsigned char value,
+    unsigned char* output,
+    int outputIndex)
+{
+    output[outputIndex] = value;
+    output[outputIndex + 1] = value;
+    output[outputIndex + 2] = value;
+}
+
+__global__ void sobelKernel(
+    int width,
+    int height,
+    const unsigned char* input,
+    unsigned char* output)
+{
+    const int x = blockIdx.x * blockDim.x + threadIdx.x;
+    const int y = blockIdx.y * blockDim.y + threadIdx.y;
+    if (x >= width || y >= height)
+    {
+        return;
+    }
+
+}
+
+}
 void launchEdgeDetection(ConstImageView input, ImageView output, cudaStream_t stream)
 {
     (void)input;
